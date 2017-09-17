d961c1cd997cada9dd07b584b662a067a5114e04
ezambomsantana <ezambomsantana@gmail.com>
2017-09-16 21:11:04 -0300

    Parking reader

---
+++ src/main/java/com/santana/interscsimulator/odreader/EstacionamentoReader.java
@@ -0,0 +1,83 @@
+package com.santana.interscsimulator.odreader;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.xpath.XPathExpressionException;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+/**
+ * 
+ * This file reads an open street maps file and saves all the nodes and links to a PostgreSQL database.
+ * It is done to use the find the closest point in a graph method from the database.
+ * 
+ * @author ezamb
+ *
+ */
+
+public class EstacionamentoReader {
+	
+	private static final String inputFile = "/home/eduardo/entrada/estacionamentos.xml";
+	private static final String outputFile = "/home/eduardo/entrada/park.xml";
+	
+	public static void main(String args[])
+			throws ParserConfigurationException, SAXException, IOException, XPathExpressionException {
+
+		// Maph file
+		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+		DocumentBuilder builder = factory.newDocumentBuilder();
+		Document docMap = builder.parse(inputFile);
+		
+		PrintWriter writer = new PrintWriter(outputFile, "UTF-8");
+
+	    writer.println("<park>");	 
+	    
+	    StringBuilder sb = new StringBuilder();
+		
+		docMap.getDocumentElement().normalize();
+		NodeList nList = docMap.getElementsByTagName("spot");
+
+		for (int temp = 0; temp < nList.getLength(); temp++) {
+
+			Node nNode = nList.item(temp);
+
+			if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+								
+				Element eElement = (Element) nNode;
+
+				String id = eElement.getElementsByTagName("uuid").item(0).getTextContent();
+				Node coordinates = eElement.getElementsByTagName("coordinates").item(0);
+				
+				String lat = ((Element) coordinates).getElementsByTagName("lat").item(0).getTextContent();
+				String lon = ((Element) coordinates).getElementsByTagName("lon").item(0).getTextContent();
+				
+				sb.append("    <spot uuid=\"");
+				sb.append(id);
+				sb.append("\" lat=\"");
+				sb.append(lat);
+				sb.append("\" lon=\"");
+				sb.append(lon);
+				sb.append("\" />\n");
+				
+				
+			}
+
+		}
+		
+		writer.println(sb.toString());
+
+	    writer.println("</park>");
+	    writer.close();	
+		
+
+	}
+
+}
