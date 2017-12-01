bb593860890f43f8ef153f5a189757233c2fa360
ezambomsantana <ezambomsantana@gmail.com>
2017-12-01 18:32:26 -0200

    Commit class Car

---
+++ src/main/java/com/santana/interscsimulator/entity/Car.java
@@ -0,0 +1,42 @@
+package com.santana.interscsimulator.entity;
+
+public class Car {
+	
+	private int id;
+	private String time;
+	private String carId;
+	private String lat;
+	private String lon;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getTime() {
+		return time;
+	}
+	public void setTime(String time) {
+		this.time = time;
+	}
+	public String getCarId() {
+		return carId;
+	}
+	public void setCarId(String carId) {
+		this.carId = carId;
+	}
+	public String getLat() {
+		return lat;
+	}
+	public void setLat(String lat) {
+		this.lat = lat;
+	}
+	public String getLon() {
+		return lon;
+	}
+	public void setLon(String lon) {
+		this.lon = lon;
+	}
+
+}
